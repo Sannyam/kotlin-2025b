7fc15483986ad6f0b51a446cb25953d7abfe4cf3
Leebongjun <bbong7019@naver.com>
2025-12-12 15:10:50 +0900

    week14 : Network App v0.1

--- AppWeek14/app/build.gradle.kts
+++ AppWeek14/app/build.gradle.kts
@@ -49,6 +49,13 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    // 코루틴, 레트로핏 초기화
+    //Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+    //Retrofit
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.converter.gson)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
